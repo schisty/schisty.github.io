3368b4aa377d8f0e6a4203b763e7897ccb58a2a2
schisty <obtester@mail.ru>
2017-04-15 12:54:32 +0300

    back update

--- classwork/phonebookclasswork.php
+++ classwork/phonebookclasswork.php
@@ -1,4 +1,4 @@
-<form><a class="btn" href="/schisty/index.html">Back</a></form> 
+<div><a class="btn" href="https://www.schisty.github.io/">Back</a></div> 
 
 <?php
 $ljudi=array(); //Создали общий массив
